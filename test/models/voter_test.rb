55f1aa666350491873e7018725a42de95a6dbdcd
Aaron Wiggins <apw7177@gmail.com>
2015-05-26 19:47:05 -0400

    create tests for voter

--- test/models/voter_test.rb
+++ test/models/voter_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class VoterTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "check validation" do
+    legolas = Voter.create(name: "Legolas", party: "Elves")
+    aragorn = Voter.new(name: "Legolas", party: "Men")
+
+    refute aragorn.save
+  end
+
+  test "check association" do
+    gimli = Candidate.create(name: "Gimli", hometown: "Misty Mountains", district: "Glittering Caves", party: "Dwarves")
+    legolas = Voter.create(name: "Legolas", party: "Elves")
+    vote = Vote.create(candidate_id: gimli.id, voter_id: legolas.id)
+
+    assert_equal vote.voter_id, Voter.last.id
   end
 end
