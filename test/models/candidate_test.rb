efc50de43261c94f3284012e9615b17e061660a1
Aaron Wiggins <apw7177@gmail.com>
2015-05-26 15:59:00 -0400

    fix spelling error. pass 2 tests for candidates

--- test/models/candidate_test.rb
+++ test/models/candidate_test.rb
@@ -5,8 +5,15 @@ class CandidateTest < ActiveSupport::TestCase
     assert true
   end
 
-  test "create candiate" do
+  test "create candidate" do
     bill = Candidate.new(name: "Bill Murray", hometown: "Evanston, IL", district: "13")
     refute bill.save
+  end
+
+  test "get last candidate" do
+    bill = Candidate.create(name: "Bill Murray", hometown: "Evanston, IL", district: "13", party: "Busters")
+    gimli = Candidate.create(name: "Gimli", hometown: "Misty Mountains", district: "Glittering Caves", party: "Dwarves")
+
+    assert_equal gimli.name, Candidate.last.name
   end
 end
